aeb0f9536a05f593e58bd3aff30bde63758eb0d9
thiagosullivan <thiagosullivan12@gmail.com>
2026-04-19 22:35:42 -0300

    feat: add navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/navbar.tsx
@@ -0,0 +1,32 @@
+import { BriefcaseIcon } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+
+export default function Navbar() {
+  return (
+    <nav className="border-b border-gray-200 bg-white">
+      <div className="container mx-auto flex h-16 items-center px-4 justify-between">
+        <Link
+          href="/"
+          className="flex items-center space-x-2 gap-2 text-xl font-semibold text-primary"
+        >
+          <BriefcaseIcon />
+          Job Tracker
+        </Link>
+        <div className="flex items-center gap-4">
+          <Link href="/sign-in">
+            <Button variant="ghost" className="text-gray-700 hover:text-black">
+              Login
+            </Button>
+          </Link>
+          <Link href="/sign-up">
+            <Button className="bg-primary hover:bg-primary/90">
+              Start for free
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
