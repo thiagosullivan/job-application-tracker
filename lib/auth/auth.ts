a6a56d70962c4eeee6161e3c1e36e0580a78f642
thiagosullivan <thiagosullivan12@gmail.com>
2026-04-26 18:51:32 -0300

    fix: changes components from baseui to radixui on shadcn

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 const client = new MongoClient(process.env.MONGODB_URI!);
 const db = client.db();
@@ -21,4 +22,14 @@ export async function getSession() {
   });
 
   return result;
+}
+
+export async function signOut() {
+  const result = await auth.api.signOut({
+    headers: await headers(),
+  });
+
+  if (result.success) {
+    redirect("/sign-in");
+  }
 }
