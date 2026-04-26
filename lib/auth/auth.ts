8f0cc38cda8699021c46837aeda4e59dcb7fd0d1
thiagosullivan <thiagosullivan12@gmail.com>
2026-04-26 18:19:40 -0300

    feat: get sessions from header

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
+import { headers } from "next/headers";
 
 const client = new MongoClient(process.env.MONGODB_URI!);
 const db = client.db();
@@ -13,3 +14,11 @@ export const auth = betterAuth({
     enabled: true,
   },
 });
+
+export async function getSession() {
+  const result = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  return result;
+}
