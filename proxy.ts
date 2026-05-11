4e44c8c422bcd03af16529df32832fe12c07470d
thiagosullivan <thiagosullivan12@gmail.com>
2026-05-11 17:02:12 -0300

    feat: middleware to desible signin/signup pages when user is logged

--- proxy.ts
+++ proxy.ts
@@ -9,5 +9,11 @@ export default async function proxy(request: NextRequest) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
+  const isSignInPage = request.nextUrl.pathname.startsWith("/sign-in");
+  const isSignUpPage = request.nextUrl.pathname.startsWith("/sign-up");
+  if ((isSignInPage || isSignUpPage) && session?.user) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
   return NextResponse.next();
 }
