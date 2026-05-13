ebd55de8cbc4005e83b1e77db627625d9613205c
thiagosullivan <thiagosullivan12@gmail.com>
2026-05-12 23:51:32 -0300

    feat: getting board data on dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,24 @@
+import { getSession } from "@/lib/auth/auth";
+import connectDB from "@/lib/db";
+import { Board } from "@/lib/models";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const session = await getSession();
+
+  if (!session?.user) {
+    redirect("/sign-in");
+  }
+
+  await connectDB();
+
+  const board = await Board.findOne({
+    userId: session.user.id,
+    name: "Job Hunt",
+  });
+
+  console.log(board);
+
   return <div>Dashboard</div>;
 }
--- proxy.ts
+++ proxy.ts
@@ -4,10 +4,10 @@ import { getSession } from "./lib/auth/auth";
 export default async function proxy(request: NextRequest) {
   const session = await getSession();
 
-  const isDashboardPage = request.nextUrl.pathname.startsWith("/dashboard");
-  if (isDashboardPage && !session?.user) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
-  }
+  // const isDashboardPage = request.nextUrl.pathname.startsWith("/dashboard");
+  // if (isDashboardPage && !session?.user) {
+  //   return NextResponse.redirect(new URL("/sign-in", request.url));
+  // }
 
   const isSignInPage = request.nextUrl.pathname.startsWith("/sign-in");
   const isSignUpPage = request.nextUrl.pathname.startsWith("/sign-up");
